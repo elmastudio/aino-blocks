30d6fcd157ec3325d7d745e3a3caff98e237180f
Ellen Bauer <ellen.bauer@elmastudio.de>
2019-08-11 12:47:31 +0700

    Add Gitignore file.

--- src/blocks/advanced-button/attributes.js
+++ src/blocks/advanced-button/attributes.js
@@ -1,4 +1,36 @@
 const attributes = {
+	url: {
+		"type": "string",
+		"source": "attribute",
+		"selector": "a",
+		"attribute": "href"
+	},
+	title: {
+		"type": "string",
+		"source": "attribute",
+		"selector": "a",
+		attribute": "title"
+	},
+	text: {
+		type: "string",
+		source: "html",
+		selector": "a"
+	},
+	linkTarget: {
+		type: "string",
+		source: "attribute",
+		selector: "a",
+		attribute: "target"
+	},
+	rel: {
+		type: "string",
+		source: "attribute",
+		selector: "a",
+		attribute: "rel"
+	},
+	placeholder: {
+		type: "string"
+	}
 	size: {
 		type: 'string',
 		default: 'size-m'
