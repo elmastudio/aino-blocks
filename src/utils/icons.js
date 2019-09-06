3fb0245635bb7280ae4f1c1516a014a8b8bd0382
Ellen Bauer <ellen.bauer@elmastudio.de>
2019-09-06 22:09:51 +1200

    Update icons.js

--- src/utils/icons.js
+++ src/utils/icons.js
@@ -4,34 +4,52 @@
 const icons = {};
 
 icons.categoryIcon =
-	<svg className="components-ainoblocks-logo-icon components-ainoblocks-svg" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"><path fill="#f52562" d="M6.41 0h7.181c2.229 0 3.037 0.232 3.852 0.668s1.454 1.075 1.89 1.89c0.436 0.815 0.668 1.623 0.668 3.852v7.181c0 2.229-0.232 3.037-0.668 3.852s-1.075 1.454-1.89 1.89c-0.815 0.436-1.623 0.668-3.852 0.668h-7.181c-2.229 0-3.037-0.232-3.852-0.668s-1.454-1.075-1.89-1.89c-0.436-0.815-0.668-1.623-0.668-3.852v-7.181c0-2.229 0.232-3.037 0.668-3.852s1.075-1.454 1.89-1.89c0.815-0.436 1.623-0.668 3.852-0.668z"></path>
-		<path fill="#fff" d="M12.018 5.337l4.039 6.995c0.198 0.343 0.312 0.741 0.312 1.166 0 1.287-1.043 2.33-2.33 2.33-0.863 0-1.616-0.469-2.019-1.165l-4.039-6.995c-0.198-0.343-0.312-0.741-0.312-1.166 0-1.287 1.043-2.33 2.33-2.33 0.863 0 1.616 0.469 2.019 1.165zM5.962 15.828c-1.287 0-2.331-1.043-2.331-2.331s1.043-2.331 2.331-2.331c1.287 0 2.331 1.043 2.331 2.331s-1.043 2.331-2.331 2.331z"></path></svg>
+	<svg className="components-ainoblocks-logo-icon components-ainoblocks-svg" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 26 20"><path fill="#ec3f50" d="M15.484 0l-9.677 16.774h19.355z"></path>
+		<path fill="#710c3a" d="M9.677 3.226l-9.677 16.774h19.355l-9.677-16.774zM17.121 18.71h-14.887l7.443-12.902 7.443 12.902z"></path>
+		<path fill="#710c3a" d="M11.65 6.645l5.843 10.128h-1.49l-5.098-8.837 0.745-1.291z"></path></svg>
 
 icons.card =
-	<svg className="components-ainoblocks-svg" aria-hidden role="img" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="#f52562"
-		d="M2 22h18c0 1.105-0.895 2-2 2h-16c-1.105 0-2-0.895-2-2v-16c0-1.105 0.895-2 2-2v18zM22 0c1.105 0 2 0.895 2 2v16c0 1.105-0.895 2-2 2h-16c-1.105 0-2-0.895-2-2v-16c0-1.105 0.895-2 2-2h16zM22 2h-16v16h16v-16zM13 9v-3c0-0.552 0.448-1 1-1s1 0.448 1 1v3h3c0.552 0 1 0.448 1 1s-0.448 1-1 1h-3v3c0 0.552-0.448 1-1 1s-1-0.448-1-1v-3h-3c-0.552 0-1-0.448-1-1s0.448-1 1-1h3z">
-	</path></svg>
+	<svg className="components-ainoblocks-svg" aria-hidden role="img" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="#fd9491" d="M4 0h20v20h-20v-20z"></path>
+		<path fill="none" stroke="#791b39" stroke-linejoin="miter"
+			stroke-linecap="butt" stroke-miterlimit="4" stroke-width="2" d="M1 5h18v18h-18v-18z"></path></svg>
 
 icons.author =
-	<svg className="components-ainoblocks-svg" aria-hidden role="img" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="#f52562"
-		d="M4 8c2.209 0 4 1.791 4 4s-1.791 4-4 4c-2.209 0-4-1.791-4-4s1.791-4 4-4zM11 9h12c0.552 0 1 0.448 1 1s-0.448 1-1 1h-12c-0.552 0-1-0.448-1-1s0.448-1 1-1zM11 13h10c0.552 0 1 0.448 1 1s-0.448 1-1 1h-10c-0.552 0-1-0.448-1-1s0.448-1 1-1z">
-	</path></svg>
+	<svg className="components-ainoblocks-svg" aria-hidden role="img" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="#8ecc93"
+		d="M24 10c0 5.523-4.477 10-10 10s-10-4.477-10-10c0-5.523 4.477-10 10-10s10 4.477 10 10z"></path>
+		<path fill="none" stroke="#1b613a" stroke-linejoin="miter" stroke-linecap="butt" stroke-miterlimit="4"
+			stroke-width="2" d="M19 14c0 4.971-4.029 9-9 9s-9-4.029-9-9c0-4.971 4.029-9 9-9s9 4.029 9 9z">
+		</path></svg>
 
 icons.testimonial =
-	<svg className="components-ainoblocks-svg" aria-hidden role="img" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="#f52562"
-		d="M5 0h14c2.761 0 5 2.239 5 5v14c0 2.761-2.239 5-5 5h-14c-2.761 0-5-2.239-5-5v-14c0-2.761 2.239-5 5-5zM5 2c-1.657 0-3 1.343-3 3v14c0 1.657 1.343 3 3 3h14c1.657 0 3-1.343 3-3v-14c0-1.657-1.343-3-3-3h-14zM7 15c1.105 0 2 0.895 2 2s-0.895 2-2 2c-1.105 0-2-0.895-2-2s0.895-2 2-2zM12 16h6c0.552 0 1 0.448 1 1s-0.448 1-1 1h-6c-0.552 0-1-0.448-1-1s0.448-1 1-1zM6 6h12c0.552 0 1 0.448 1 1s-0.448 1-1 1h-12c-0.552 0-1-0.448-1-1s0.448-1 1-1zM6 10h10c0.552 0 1 0.448 1 1s-0.448 1-1 1h-10c-0.552 0-1-0.448-1-1s0.448-1 1-1z">
-	</path></svg>
+	<svg className="components-ainoblocks-svg" aria-hidden role="img" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="#fdc3af"
+		d="M24 0v20l-10-4.444-10 4.444v-20l9.999 4.444z"></path>
+		<path fill="none" stroke="#792123" stroke-linejoin="miter"
+			stroke-linecap="butt" stroke-miterlimit="4" stroke-width="2"
+			d="M19 5.539l-9.001 4-8.999-4v16.922l9-4 9 4v-16.923z"></path></svg>
 
 icons.featuredContent =
-	<svg className="components-ainoblocks-svg" aria-hidden role="img" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="#f52562"
-		d="M19 9c-0.552 0-1-0.448-1-1s0.448-1 1-1h4c0.552 0 1 0.448 1 1s-0.448 1-1 1h-4zM19 13c-0.552 0-1-0.448-1-1s0.448-1 1-1h4c0.552 0 1 0.448 1 1s-0.448 1-1 1h-4zM19 17c-0.552 0-1-0.448-1-1s0.448-1 1-1h4c0.552 0 1 0.448 1 1s-0.448 1-1 1h-4zM6 10c-1.105 0-2-0.895-2-2s0.895-2 2-2c1.105 0 2 0.895 2 2s-0.895 2-2 2zM12 18h-8v-2l2.5-3 1.658 1.842 3.842-3.842v7zM2 2h12c1.105 0 2 0.895 2 2v16c0 1.105-0.895 2-2 2h-12c-1.105 0-2-0.895-2-2v-16c0-1.105 0.895-2 2-2zM2 4v16h12v-16h-12z">
-	</path></svg>
+	<svg className="components-ainoblocks-svg" aria-hidden role="img" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="#f8a0b8" d="M4 20c0.489-9 5.444-20 10-20s10 11 10 20h-20z"></path>
+		<path fill="none" stroke="#771e59" stroke-linejoin="miter" stroke-linecap="butt" stroke-miterlimit="4"
+			stroke-width="2" d="M1.075 23h17.902c-0.377-8.347-5.328-18-8.977-18-3.491 0-8.135 9.344-8.925 18z">
+		</path></svg>
 
 icons.advancedButton =
-	<svg className="components-ainoblocks-svg" aria-hidden role="img" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="#f52562" d="M6 6h12c3.314 0 6 2.686 6 6s-2.686 6-6 6h-12c-3.314 0-6-2.686-6-6s2.686-6 6-6zM6 8c-2.209 0-4 1.791-4 4s1.791 4 4 4h12c2.209 0 4-1.791 4-4s-1.791-4-4-4h-12zM8 11h8c0.552 0 1 0.448 1 1s-0.448 1-1 1h-8c-0.552 0-1-0.448-1-1s0.448-1 1-1z"></path></svg>
+	<svg className="components-ainoblocks-svg" aria-hidden role="img" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="#a275b0"
+		d="M6.164 2.164c2.885-2.885 7.563-2.885 10.448 0l5.224 5.224c2.885 2.885 2.885 7.563 0 10.448s-7.563 2.885-10.448 0l-5.224-5.224c-2.885-2.885-2.885-7.563 0-10.448z">
+	</path>
+		<path fill="none" stroke="#331654" stroke-linejoin="miter" stroke-linecap="butt" stroke-miterlimit="4"
+			stroke-width="2"
+			d="M2.871 6.871c-2.495 2.495-2.495 6.539 0 9.034l5.224 5.224c2.495 2.495 6.539 2.495 9.034 0s2.495-6.539 0-9.034l-5.224-5.224c-2.495-2.495-6.539-2.495-9.034 0z">
+		</path></svg>
 
 icons.advancedButtons =
-	<svg className="components-ainoblocks-svg" aria-hidden role="img" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="#f52562" d="M7 0h10c2.761 0 5 2.239 5 5s-2.239 5-5 5h-10c-2.761 0-5-2.239-5-5s2.239-5 5-5zM7 2c-1.657 0-3 1.343-3 3s1.343 3 3 3h10c1.657 0 3-1.343 3-3s-1.343-3-3-3h-10zM6 12h12c3.314 0 6 2.686 6 6s-2.686 6-6 6h-12c-3.314 0-6-2.686-6-6s2.686-6 6-6zM8 17c-0.552 0-1 0.448-1 1s0.448 1 1 1h8c0.552 0 1-0.448 1-1s-0.448-1-1-1h-8zM8 4h8c0.552 0 1 0.448 1 1s-0.448 1-1 1h-8c-0.552 0-1-0.448-1-1s0.448-1 1-1z"></path></svg>
+	<svg className="components-ainoblocks-svg" aria-hidden role="img" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="#a275b0"
+		d="M6.164 2.164c2.885-2.885 7.563-2.885 10.448 0l5.224 5.224c2.885 2.885 2.885 7.563 0 10.448s-7.563 2.885-10.448 0l-5.224-5.224c-2.885-2.885-2.885-7.563 0-10.448z">
+	</path>
+		<path fill="none" stroke="#331654" stroke-linejoin="miter" stroke-linecap="butt" stroke-miterlimit="4"
+			stroke-width="2"
+			d="M2.871 6.871c-2.495 2.495-2.495 6.539 0 9.034l5.224 5.224c2.495 2.495 6.539 2.495 9.034 0s2.495-6.539 0-9.034l-5.224-5.224c-2.495-2.495-6.539-2.495-9.034 0z">
+		</path></svg>
 
 icons.icon =
 	<svg className="components-ainoblocks-svg" aria-hidden role="img" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="#f52562"
@@ -40,5 +58,18 @@ icons.icon =
 
 icons.iconText =
 	<svg className="components-ainoblocks-svg" aria-hidden role="img" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="#f52562" d="M5 0h14c2.761 0 5 2.239 5 5v14c0 2.761-2.239 5-5 5h-14c-2.761 0-5-2.239-5-5v-14c0-2.761 2.239-5 5-5zM5 2c-1.657 0-3 1.343-3 3v14c0 1.657 1.343 3 3 3h14c1.657 0 3-1.343 3-3v-14c0-1.657-1.343-3-3-3h-14zM7 18h10c0.552 0 1 0.448 1 1s-0.448 1-1 1h-10c-0.552 0-1-0.448-1-1s0.448-1 1-1zM6 14h12c0.552 0 1 0.448 1 1s-0.448 1-1 1h-12c-0.552 0-1-0.448-1-1s0.448-1 1-1zM7.9 7.050c0-1.13 0.92-2.050 2.050-2.050 0.56 0 1.080 0.23 1.45 0.61l0.6 0.59 0.6-0.6c0.37-0.37 0.88-0.6 1.45-0.6 1.13 0 2.050 0.92 2.050 2.050 0 0.56-0.23 1.080-0.6 1.45l-3.5 3.5-3.5-3.5c-0.37-0.38-0.6-0.88-0.6-1.45z"></path></svg>
+
+icons.slider =
+	<svg className="components-ainoblocks-svg" aria-hidden role="img" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="#f52562" d="M21 7c0.237 0 0.454 0.082 0.625 0.22l0.082 0.073 2 2c0.181 0.181 0.293 0.431 0.293 0.707 0 0.237-0.082 0.454-0.22 0.625l-0.073 0.082-2 2c-0.181 0.181-0.431 0.293-0.707 0.293-0.552 0-1-0.448-1-1 0-0.276 0.112-0.526 0.293-0.707l1.292-1.293-1.292-1.293-0.073-0.082c-0.137-0.171-0.22-0.389-0.22-0.625 0-0.552 0.448-1 1-1z"></path>
+		<path fill="#f52562" d="M3 7c-0.237 0-0.454 0.082-0.625 0.22l-0.082 0.073-2 2c-0.181 0.181-0.293 0.431-0.293 0.707 0 0.237 0.082 0.454 0.22 0.625l0.073 0.082 2 2c0.181 0.181 0.431 0.293 0.707 0.293 0.552 0 1-0.448 1-1 0-0.276-0.112-0.526-0.293-0.707l-1.292-1.293 1.292-1.293 0.073-0.082c0.137-0.171 0.22-0.389 0.22-0.625 0-0.552-0.448-1-1-1z"></path>
+		<path fill="none" stroke="#f52562" stroke-linejoin="miter" stroke-linecap="butt" stroke-miterlimit="4" stroke-width="2" d="M9 5h6c1.105 0 2 0.895 2 2v6c0 1.105-0.895 2-2 2h-6c-1.105 0-2-0.895-2-2v-6c0-1.105 0.895-2 2-2z"></path>
+		<path fill="#f52562" d="M14 18c0.552 0 1 0.448 1 1s-0.448 1-1 1c-0.552 0-1-0.448-1-1s0.448-1 1-1z"></path>
+		<path fill="#f52562" d="M10 18c0.552 0 1 0.448 1 1s-0.448 1-1 1c-0.552 0-1-0.448-1-1s0.448-1 1-1z"></path></svg>
+
+icons.hero =
+	<svg className="components-ainoblocks-svg" aria-hidden role="img" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="#f52562" d="M5 0h14c2.761 0 5 2.239 5 5v14c0 2.761-2.239 5-5 5h-14c-2.761 0-5-2.239-5-5v-14c0-2.761 2.239-5 5-5zM5 2c-1.657 0-3 1.343-3 3v14c0 1.657 1.343 3 3 3h14c1.657 0 3-1.343 3-3v-14c0-1.657-1.343-3-3-3h-14z"></path>
+		<path fill="#f52562" d="M7 14h4c1.105 0 2 0.895 2 2s-0.895 2-2 2h-4c-1.105 0-2-0.895-2-2s0.895-2 2-2z"></path>
+		<path fill="#f52562" d="M6 10h10c0.552 0 1 0.448 1 1s-0.448 1-1 1h-10c-0.552 0-1-0.448-1-1s0.448-1 1-1z"></path>
+		<path fill="#f52562" d="M6 6h12c0.552 0 1 0.448 1 1s-0.448 1-1 1h-12c-0.552 0-1-0.448-1-1s0.448-1 1-1z"></path></svg>
 
 export default icons;
