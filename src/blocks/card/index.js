36b4c453c9c1b1ea1377626fc98431b5e69d7870
Ellen Bauer <ellen.bauer@elmastudio.de>
2019-09-06 22:10:42 +1200

    Update index.js

--- src/blocks/card/index.js
+++ src/blocks/card/index.js
@@ -8,9 +8,9 @@ import classnames from "classnames";
  */
 import icons from './../../utils/icons';
 import edit from './edit';
-import attributes from "./attributes";
+import attributes from './attributes';
 import save from './save';
-import "./styles/style.scss";
+import './styles/style.scss';
 import './styles/editor.scss';
 
 /**
@@ -27,16 +27,16 @@ const {
 /**
  * Register block
  */
-export default registerBlockType("ainoblocks/card", {
-	title: __("Card", "ainoblocks"),
-	description: __("This is a card block.", "ainoblocks"),
+export default registerBlockType('ainoblocks/card', {
+	title: __('Card', 'ainoblocks'),
+	description: __('This is a card block.', 'ainoblocks'),
 	icon: icons.card,
 	keywords: [
-		__("card", "ainoblocks"),
-		__("background", "ainoblocks"),
-		__("aino blocks", "ainoblocks")
+		__('card', 'ainoblocks'),
+		__('background', 'ainoblocks'),
+		__('aino blocks', 'ainoblocks')
 	],
-	category: "ainoblocks",
+	category: 'ainoblocks',
 	attributes,
 	getEditWrapperProps({ containerWidth }) {
 		if ('wide' === containerWidth || 'full' === containerWidth) {
