d654fcb3bc3adf8c0d6f572b2a37ee9c7a308097
Ellen Bauer <ellen.bauer@elmastudio.de>
2020-04-24 18:43:24 +1200

    Hero block Heading font update

--- src/blocks/hero/edit.js
+++ src/blocks/hero/edit.js
@@ -48,7 +48,7 @@ const ALLOWED_BLOCKS = ['core/image', 'core/heading', 'core/paragraph', 'ainoblo
 
 const TEMPLATE = [
 	['ainoblocks/badge', { placeholder: _x('Write Badge text…', 'content placeholder'), content: _x('Write Badge text…', 'content placeholder') }],
-	['core/heading', { placeholder: _x('Write heading…', 'content placeholder'), content: _x('Write heading…', 'content placeholder'), className: 'hero-display', }],
+	['core/heading', { placeholder: _x('Write heading…', 'content placeholder'), content: _x('Write heading…', 'content placeholder'), }],
 	['core/paragraph', { placeholder: _x('Start writing here…', 'content placeholder'), content: _x('Start writing here…', 'content placeholder'), className: 'hero-text', }],
 	['ainoblocks/multiple-buttons'],
 ];
