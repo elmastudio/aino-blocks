7c75fffe109be1aa870a1f459c4d61210c828b44
Ellen Bauer <ellen.bauer@elmastudio.de>
2020-05-23 12:58:03 +1200

    Enhance block icons and Aino logo

--- readme.txt
+++ readme.txt
@@ -19,6 +19,7 @@ Gutenberg blocks for site building.
 
 = 1.0.0 =
 New: Grid container block, which brings new possibility to build layouts with the help of a CSS grid.
+Enhance: Block icons.
 
 = 0.9.0 =
 Enhance: Expanded Hero block media settings and moved setting to inspector.
--- src/blocks/grid-container/icon.js
+++ src/blocks/grid-container/icon.js
@@ -4,5 +4,5 @@
 const { Path, SVG } = wp.components;
 
 export default <SVG className="components-ainoblocks-svg" aria-hidden role="img" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-<path fill="#7946f3" d="M15 1v3h3v7h3v-2.997h2v4.997h-2v3h-3v2h-3v5h-2v-5h-3v5h-2v-5h-2v-5h-5v-2h5v-2h-5v-3h5v-2h2v-3h7zM16 15v-2h-5v2h5zM10 6h-2v2h2v-2z"></path>
+<path fill="#7946f3" d="M10.998 24l0.002-4h-9v-11h3v8h3v-12h2.97l-0.007-5h8.037v2h3v6h-6v3h6v3h-6v7h6v3h-11.002zM16 3h-2v2h2v-2z"></path>
 </SVG>;
