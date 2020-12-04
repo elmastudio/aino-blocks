32bf884d74071e276b861d4869004896c07d7532
Ellen Bauer <ellen.bauer@elmastudio.de>
2020-12-04 17:07:03 +1300

    New: Divider block

--- src/blocks/divider/edit.js
+++ src/blocks/divider/edit.js
@@ -102,10 +102,12 @@ class dividerEdit extends Component {
 					>
 					</PanelColorSettings>
 				</InspectorControls>
-				<div className={classnames(className)} style={styles}>
-					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" class="__icon" style={iconStyles}>
-						<path d="M25.586 6.586l17.414 17.414-17.414 17.414-2.828-2.828 12.585-12.586-30.342 0v-4l30.342-0-12.585-12.586z"></path>
-					</svg>
+				<div className={classnames(className)}>
+					<span class="wp-block-ainoblocks-divider__inner" style={ styles }>
+						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" class="__icon" style={iconStyles}>
+							<path d="M25.586 6.586l17.414 17.414-17.414 17.414-2.828-2.828 12.585-12.586-30.342 0v-4l30.342-0-12.585-12.586z"></path>
+						</svg>
+					</span>
 				</div>
 			</Fragment>
 		);
