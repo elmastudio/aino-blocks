19d7a80cb6c5dcd365dbfd5c85b0ece1d1c56cd4
Ellen Bauer <ellen.bauer@elmastudio.de>
2021-08-23 14:09:12 +1200

    Block icons update

--- src/blocks/card/icon.js
+++ src/blocks/card/icon.js
@@ -5,5 +5,7 @@ const { Path, SVG } = wp.components;
 
 export default 
 <SVG className="components-ainoblocks-svg" aria-hidden role="img" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-	<path d="M0 24l7-24 5 17.143 5-17.143 7 24h-24z"></path>
+<path fill="#6af9ec" d="M12 10l12 7-12 7-12-7z"></path>
+<path fill="#09d9c7" d="M12 5l12 7-12 7-12-7z"></path>
+<path fill="#07ae9f" d="M12 0l12 7-12 7-12-7z"></path>
 </SVG>;
--- src/blocks/hero/icon.js
+++ src/blocks/hero/icon.js
@@ -5,5 +5,7 @@ const { Path, SVG } = wp.components;
 
 export default 
 <SVG className="components-ainoblocks-svg" aria-hidden role="img" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-<path d="M12 0l4 12-4 12-4-12 4-12zM20 0l4 12-4 12-4-12 4-12zM4 0l4 12-4 12-4-12 4-12z"></path>
+<path fill="#00a07d" d="M23 8l-22 14h22z"></path>
+<path fill="#00795e" d="M12 2l11 20h-22z"></path>
+<path fill="#005240" d="M1 8l22 14h-22z"></path>
 </SVG>;
