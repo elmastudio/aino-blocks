eb6061143645a12be3f3af8a88b20a12f0c2e26f
Ellen Bauer <ellen.bauer@elmastudio.de>
2022-06-19 12:09:21 +1200

    Update card shadow.

--- src/blocks/card/icon.js
+++ src/blocks/card/icon.js
@@ -5,7 +5,7 @@ const { SVG } = wp.components;
 
 export default 
 <SVG className="components-ainoblocks-svg" aria-hidden role="img" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-<path fill="#ff763a" d="M24 6l-12-6-12 6 12 6 12-6z"></path>
-<path fill="#ff763a" d="M3 10.5l-3 1.5 12 6 12-6-3-1.5-9 4.5-9-4.5z"></path>
-<path fill="#ff763a" d="M3 16.5l-3 1.5 12 6 12-6-3-1.5-9 4.5-9-4.5z"></path>
+<path fill="#b91dad" d="M24 6l-12-6-12 6 12 6 12-6z"></path>
+<path fill="#b91dad" d="M3 10.5l-3 1.5 12 6 12-6-3-1.5-9 4.5-9-4.5z"></path>
+<path fill="#b91dad" d="M3 16.5l-3 1.5 12 6 12-6-3-1.5-9 4.5-9-4.5z"></path>
 </SVG>;
