f445d2b44780b5eaf1fb0510997e6c101109c3e9
Ellen Bauer <ellen.bauer@elmastudio.de>
2022-08-05 17:50:48 +1200

    Grid Item block update

--- build/grid-item/index.asset.php
+++ build/grid-item/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => '1675ffdf2dd996b234ac');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => '89a1f6b4a5819cb36399');
