bd8b9d037da77665cdc54880ddc412195e601cb8
Ellen Bauer <ellen.bauer@elmastudio.de>
2022-10-21 16:27:07 +1300

    Icon block update

--- build/icon/index.asset.php
+++ build/icon/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '82fe1a197dd268a8f5ad');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '9a459b45303c6db0f0db');
