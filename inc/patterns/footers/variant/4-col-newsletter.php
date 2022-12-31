a82fa85c4f2b6feec5bc0ab454ed7890defe051c
Ellen Bauer <ellen.bauer@elmastudio.de>
2022-12-31 23:26:05 +1300

    New patterns

---
+++ inc/patterns/footers/variant/4-col-newsletter.php
@@ -0,0 +1,92 @@
+<?php
+return array(
+	'title'      => __( 'Footer 4 col with newsletter, variant', 'abpatterns' ),
+	'categories' => array( 'footers' ),
+	'content'    => '<!-- wp:group {"align":"full","backgroundColor":"variant-background-primary","className":"site-footer"} -->
+	<div class="wp-block-group alignfull site-footer has-variant-background-primary-background-color has-background"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|12","bottom":"var:preset|spacing|12"}}},"layout":{"type":"constrained"}} -->
+	<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--12);padding-bottom:var(--wp--preset--spacing--12)"><!-- wp:ainoblocks/grid-container {"columnGap":"col-gap-m","rowGap":"row-gap-xxl"} -->
+	<div class="wp-block-ainoblocks-grid-container alignwide ab-grid-block col-gap-m row-gap-xxl"><!-- wp:ainoblocks/grid-item {"gridColumnStartDesktop":1,"gridColumnEndDesktop":3,"gridColumnStartTablet":1,"gridColumnEndTablet":5,"gridColumnStartMobile":1,"gridColumnEndMobile":13,"justifyItem":"stretch","className":"align-self__start"} -->
+	<div class="wp-block-ainoblocks-grid-item col_start_d__1 col_end_d__3 col_start_t__1 col_end_t__5 col_start_m__1 col_end_m__13 justify-self__stretch no-stacking align-self__start"><!-- wp:heading {"textAlign":"left","level":3,"style":{"typography":{"lineHeight":"1.5"},"spacing":{"padding":{"bottom":"var:preset|spacing|4"}}},"textColor":"variant-font-primary","fontSize":"text-xs"} -->
+	<h3 class="has-text-align-left has-variant-font-primary-color has-text-color has-text-xs-font-size" style="padding-bottom:var(--wp--preset--spacing--4);line-height:1.5">Footer Title 1</h3>
+	<!-- /wp:heading -->
+	
+	<!-- wp:navigation {"textColor":"variant-font-primary","overlayMenu":"never","layout":{"type":"flex","orientation":"vertical","flexWrap":"nowrap"},"style":{"spacing":{"blockGap":"var:preset|spacing|4"}},"fontSize":"text-xs"} /--></div>
+	<!-- /wp:ainoblocks/grid-item -->
+	
+	<!-- wp:ainoblocks/grid-item {"gridColumnStartDesktop":3,"gridColumnEndDesktop":5,"gridColumnStartTablet":5,"gridColumnEndTablet":9,"gridColumnStartMobile":1,"gridColumnEndMobile":13,"justifyItem":"stretch","className":"align-self__start"} -->
+	<div class="wp-block-ainoblocks-grid-item col_start_d__3 col_end_d__5 col_start_t__5 col_end_t__9 col_start_m__1 col_end_m__13 justify-self__stretch no-stacking align-self__start"><!-- wp:heading {"textAlign":"left","level":3,"style":{"typography":{"lineHeight":"1.5"},"spacing":{"padding":{"bottom":"var:preset|spacing|4"}}},"textColor":"variant-font-primary","fontSize":"text-xs"} -->
+	<h3 class="has-text-align-left has-variant-font-primary-color has-text-color has-text-xs-font-size" style="padding-bottom:var(--wp--preset--spacing--4);line-height:1.5">Footer Title 2</h3>
+	<!-- /wp:heading -->
+	
+	<!-- wp:navigation {"textColor":"variant-font-primary","overlayMenu":"never","layout":{"type":"flex","orientation":"vertical","flexWrap":"nowrap"},"style":{"spacing":{"blockGap":"var:preset|spacing|4"}},"fontSize":"text-xs"} /--></div>
+	<!-- /wp:ainoblocks/grid-item -->
+	
+	<!-- wp:ainoblocks/grid-item {"gridColumnStartDesktop":5,"gridColumnEndDesktop":7,"gridColumnStartTablet":9,"gridColumnEndTablet":13,"gridColumnStartMobile":1,"gridColumnEndMobile":13,"justifyItem":"stretch","className":"align-self__start"} -->
+	<div class="wp-block-ainoblocks-grid-item col_start_d__5 col_end_d__7 col_start_t__9 col_end_t__13 col_start_m__1 col_end_m__13 justify-self__stretch no-stacking align-self__start"><!-- wp:heading {"textAlign":"left","level":3,"style":{"typography":{"lineHeight":"1.5"},"spacing":{"padding":{"bottom":"var:preset|spacing|4"}}},"textColor":"variant-font-primary","fontSize":"text-xs"} -->
+	<h3 class="has-text-align-left has-variant-font-primary-color has-text-color has-text-xs-font-size" style="padding-bottom:var(--wp--preset--spacing--4);line-height:1.5">Footer Title 3</h3>
+	<!-- /wp:heading -->
+	
+	<!-- wp:navigation {"textColor":"variant-font-primary","overlayMenu":"never","layout":{"type":"flex","orientation":"vertical","flexWrap":"nowrap"},"style":{"spacing":{"blockGap":"var:preset|spacing|4"}},"fontSize":"text-xs"} /--></div>
+	<!-- /wp:ainoblocks/grid-item -->
+	
+	<!-- wp:ainoblocks/grid-item {"gridColumnStartDesktop":9,"gridColumnEndDesktop":13,"gridColumnStartTablet":1,"gridColumnEndTablet":8,"gridColumnStartMobile":1,"gridColumnEndMobile":13,"justifyItem":"stretch","marginTopDesktop":0,"marginTopTablet":9,"marginBottomTablet":0,"marginTopMobile":0,"marginBottomMobile":0,"className":"align-self__start btn-rounded form-rounded btn-s form-s form-variant btn-style-variant"} -->
+	<div class="wp-block-ainoblocks-grid-item col_start_d__9 col_end_d__13 col_start_t__1 col_end_t__8 col_start_m__1 col_end_m__13 justify-self__stretch no-stacking mt_d__0 mt_t__9 mb_t__0 mt_m__0 mb_m__0 align-self__start btn-rounded form-rounded btn-s form-s form-variant btn-style-variant"><!-- wp:heading {"textAlign":"left","level":3,"style":{"typography":{"lineHeight":"1.5"},"spacing":{"padding":{"bottom":"var:preset|spacing|4"}}},"textColor":"variant-font-primary","fontSize":"text-xs"} -->
+	<h3 class="has-text-align-left has-variant-font-primary-color has-text-color has-text-xs-font-size" style="padding-bottom:var(--wp--preset--spacing--4);line-height:1.5">Subscribe to our newsletter</h3>
+	<!-- /wp:heading -->
+	
+	<!-- wp:paragraph {"style":{"spacing":{"padding":{"bottom":"var:preset|spacing|4"}}},"textColor":"variant-font-secondary","fontSize":"text-xs"} -->
+	<p class="has-variant-font-secondary-color has-text-color has-text-xs-font-size" style="padding-bottom:var(--wp--preset--spacing--4)">Lorem ipsum dolor sit amet consectetur. Id suspendisse faucibus tincidunt purus at a curabitur tempor. Nibh pellentesque mauris laoreet sagittis nisl feugiat sollicitudin interdum.</p>
+	<!-- /wp:paragraph -->
+	
+	<!-- wp:mailchimp-for-wp/form {} /--></div>
+	<!-- /wp:ainoblocks/grid-item --></div>
+	<!-- /wp:ainoblocks/grid-container -->
+	
+	<!-- wp:ainoblocks/grid-container {"columnGap":"col-gap-m","rowGap":"row-gap-xl","paddingTop":13,"paddingBottom":1} -->
+	<div class="wp-block-ainoblocks-grid-container alignwide ab-grid-block col-gap-m row-gap-xl pt__13 pb__1"><!-- wp:ainoblocks/grid-item {"gridColumnStartDesktop":1,"gridColumnEndDesktop":6,"gridColumnStartTablet":1,"gridColumnEndTablet":13,"gridColumnStartMobile":1,"gridColumnEndMobile":13,"alignItem":"start","justifyItem":"stretch","justifyItemTablet":"stretch","justifyItemMobile":"stretch","marginTopDesktop":0,"marginTopMobile":0,"paddingTop":1} -->
+	<div class="wp-block-ainoblocks-grid-item col_start_d__1 col_end_d__6 col_start_t__1 col_end_t__13 col_start_m__1 col_end_m__13 align-self__start justify-self__stretch js__t__stretch js__m__stretch no-stacking mt_d__0 mt_m__0 pt__1"><!-- wp:ainoblocks/flexbox {"flexDirectionTablet":"direction__col__t","flexDirectionMobile":"direction__col__m","alignItemsTablet":"align-items__flexstart__t","alignItemsMobile":"align-items__flexstart__m","alignContentTablet":"align-cont__flexstart__t","alignContentMobile":"align-cont__flexstart__m","gap":"gap-xl"} -->
+	<div class="wp-block-ainoblocks-flexbox direction__row__d direction__col__t direction__col__m nowrap__d nowrap_t nowrap_m justify__flexstart__d justify__flexstart__t justify__flexstart__m align-items__stretch__d align-items__flexstart__t align-items__flexstart__m align-cont__stretch__d align-cont__flexstart__t align-cont__flexstart__m shadow-none gap-xl"><!-- wp:ainoblocks/flex-item -->
+	<div class="wp-block-ainoblocks-flex-item auto"><!-- wp:ainoblocks/flexbox {"alignItemsDesktop":"align-items__center__d","alignItemsTablet":"align-items__center__t","alignItemsMobile":"align-items__center__m"} -->
+	<div class="wp-block-ainoblocks-flexbox direction__row__d direction__row__t direction__row__m nowrap__d nowrap_t nowrap_m justify__flexstart__d justify__flexstart__t justify__flexstart__m align-items__center__d align-items__center__t align-items__center__m align-cont__stretch__d align-cont__stretch__t align-cont__stretch__m shadow-none"><!-- wp:ainoblocks/flex-item {"flexShrink":true,"alignSelfDesktop":"align__auto__d"} -->
+	<div class="wp-block-ainoblocks-flex-item align__auto__d auto" style="flex-shrink:0"><!-- wp:site-logo {"width":24} /--></div>
+	<!-- /wp:ainoblocks/flex-item -->
+	
+	<!-- wp:ainoblocks/flex-item {"alignSelfDesktop":"align__auto__d"} -->
+	<div class="wp-block-ainoblocks-flex-item align__auto__d auto"><!-- wp:site-title {"style":{"spacing":{"padding":{"left":"0.5rem"}}},"textColor":"variant-font-primary","fontSize":"text-m"} /--></div>
+	<!-- /wp:ainoblocks/flex-item --></div>
+	<!-- /wp:ainoblocks/flexbox --></div>
+	<!-- /wp:ainoblocks/flex-item -->
+	
+	<!-- wp:ainoblocks/flex-item -->
+	<div class="wp-block-ainoblocks-flex-item auto"><!-- wp:social-links {"iconColor":"variant-font-secondary","iconColorValue":"#5e64ff","style":{"spacing":{"blockGap":{"left":"24px"}}},"className":"is-style-logos-only","layout":{"type":"flex","justifyContent":"center","flexWrap":"nowrap"}} -->
+	<ul class="wp-block-social-links has-icon-color is-style-logos-only"><!-- wp:social-link {"url":"https://twitter.com/","service":"twitter"} /-->
+	
+	<!-- wp:social-link {"url":"https://www.instagram.com/","service":"instagram"} /-->
+	
+	<!-- wp:social-link {"url":"https://github.com/","service":"github"} /-->
+	
+	<!-- wp:social-link {"url":"https://www.youtube.com/","service":"youtube"} /--></ul>
+	<!-- /wp:social-links --></div>
+	<!-- /wp:ainoblocks/flex-item --></div>
+	<!-- /wp:ainoblocks/flexbox --></div>
+	<!-- /wp:ainoblocks/grid-item -->
+	
+	<!-- wp:ainoblocks/grid-item {"gridColumnStartDesktop":7,"gridColumnEndDesktop":13,"gridColumnStartTablet":1,"gridColumnEndTablet":13,"gridColumnStartMobile":1,"gridColumnEndMobile":13,"alignItem":"start","justifyItem":"stretch","marginTopDesktop":0,"paddingTop":1} -->
+	<div class="wp-block-ainoblocks-grid-item col_start_d__7 col_end_d__13 col_start_t__1 col_end_t__13 col_start_m__1 col_end_m__13 align-self__start justify-self__stretch no-stacking mt_d__0 pt__1"><!-- wp:ainoblocks/flexbox {"flexDirectionDesktop":"direction__col__d","flexDirectionTablet":"direction__col__t","flexDirectionMobile":"direction__col__m","alignItemsDesktop":"align-items__flexend__d","alignItemsTablet":"align-items__flexstart__t","alignItemsMobile":"align-items__flexstart__m"} -->
+	<div class="wp-block-ainoblocks-flexbox direction__col__d direction__col__t direction__col__m nowrap__d nowrap_t nowrap_m justify__flexstart__d justify__flexstart__t justify__flexstart__m align-items__flexend__d align-items__flexstart__t align-items__flexstart__m align-cont__stretch__d align-cont__stretch__t align-cont__stretch__m shadow-none"><!-- wp:ainoblocks/flex-item -->
+	<div class="wp-block-ainoblocks-flex-item auto"><!-- wp:paragraph {"align":"left","textColor":"variant-font-secondary","fontSize":"text-xs"} -->
+	<p class="has-text-align-left has-variant-font-secondary-color has-text-color has-text-xs-font-size">©2022 Your name. Terms / Privacy</p>
+	<!-- /wp:paragraph --></div>
+	<!-- /wp:ainoblocks/flex-item -->
+	
+	<!-- wp:ainoblocks/flex-item -->
+	<div class="wp-block-ainoblocks-flex-item auto"><!-- wp:paragraph {"align":"left","style":{"spacing":{"padding":{"top":"var:preset|spacing|3"}}},"textColor":"variant-font-secondary","fontSize":"text-xxs","paddingTop":0,"paddingBottom":1,"paddingRight":1} -->
+	<p class="has-text-align-left has-variant-font-secondary-color has-text-color has-text-xxs-font-size pb__1 pr__1" style="padding-top:var(--wp--preset--spacing--3)">Built with <a href="https://ainoblocks.io/">Daifuku theme</a>.</p>
+	<!-- /wp:paragraph --></div>
+	<!-- /wp:ainoblocks/flex-item --></div>
+	<!-- /wp:ainoblocks/flexbox --></div>
+	<!-- /wp:ainoblocks/grid-item --></div>
+	<!-- /wp:ainoblocks/grid-container --></div>
+	<!-- /wp:group --></div>
+	<!-- /wp:group -->',
+);
---
+++ inc/patterns/heroes/variant/3-col-gallery.php
@@ -0,0 +1,64 @@
+<?php
+return array(
+	'title'      => __( '3 col gallery, variant', 'abpatterns' ),
+	'categories' => array( 'heroes' ),
+	'content'    => '<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|12","bottom":"var:preset|spacing|12"}}},"backgroundColor":"variant-background-primary","layout":{"type":"constrained"}} -->
+	<div class="wp-block-group alignfull has-variant-background-primary-background-color has-background" style="padding-top:var(--wp--preset--spacing--12);padding-bottom:var(--wp--preset--spacing--12)"><!-- wp:ainoblocks/grid-container {"columnGap":"col-gap-m","paddingBottom":10} -->
+	<div class="wp-block-ainoblocks-grid-container alignwide ab-grid-block col-gap-m pb__10"><!-- wp:ainoblocks/grid-item {"gridColumnStartDesktop":3,"gridColumnEndDesktop":11,"gridColumnStartTablet":3,"gridColumnEndTablet":11} -->
+	<div class="wp-block-ainoblocks-grid-item col_start_d__3 col_end_d__11 col_start_t__3 col_end_t__11 col_start_m__1 col_end_m__13 no-stacking"><!-- wp:heading {"textAlign":"center","textColor":"variant-font-tertiary","fontSize":"text-xs","paddingBottom":5} -->
+	<h2 class="has-text-align-center has-variant-font-tertiary-color has-text-color has-text-xs-font-size pb__5">WordPress</h2>
+	<!-- /wp:heading -->
+	
+	<!-- wp:heading {"textAlign":"center","textColor":"variant-font-primary","fontSize":"text-5-xl"} -->
+	<h2 class="has-text-align-center has-variant-font-primary-color has-text-color has-text-5-xl-font-size">WooCommerce websites made easy</h2>
+	<!-- /wp:heading -->
+	
+	<!-- wp:paragraph {"align":"center","textColor":"variant-font-secondary","fontSize":"text-m","paddingTop":5,"paddingBottom":1} -->
+	<p class="has-text-align-center has-variant-font-secondary-color has-text-color has-text-m-font-size pt__5 pb__1">Superfast website designs in WordPress.</p>
+	<!-- /wp:paragraph -->
+	
+	<!-- wp:ainoblocks/multiple-buttons {"align":"center","flexDirectionMobile":"m__row","gapDesktop":true,"gapTablet":true,"gapMobile":true,"paddingTop":8} -->
+	<div class="wp-block-ainoblocks-multiple-buttons aligncenter m__row m__gap t__gap d__gap pt__8"><!-- wp:ainoblocks/button {"mode":true,"size":"size__l","borderRadius":12,"label":"Get started","className":"is-style-primary"} -->
+	<div class="wp-block-ainoblocks-button variant is-style-primary" style="border-radius:12px"><a class="wp-block-ainoblocks-button__link size__l 12" style="border-radius:12px">Get started</a></div>
+	<!-- /wp:ainoblocks/button -->
+	
+	<!-- wp:ainoblocks/button {"mode":true,"size":"size__l","borderRadius":12,"label":"More info","className":"is-style-outline"} -->
+	<div class="wp-block-ainoblocks-button variant is-style-outline" style="border-radius:12px"><a class="wp-block-ainoblocks-button__link size__l 12" style="border-radius:12px">More info</a></div>
+	<!-- /wp:ainoblocks/button --></div>
+	<!-- /wp:ainoblocks/multiple-buttons --></div>
+	<!-- /wp:ainoblocks/grid-item --></div>
+	<!-- /wp:ainoblocks/grid-container -->
+	
+	<!-- wp:ainoblocks/grid-container {"columnGap":"col-gap-m","paddingTop":1} -->
+	<div class="wp-block-ainoblocks-grid-container alignwide ab-grid-block col-gap-m pt__1"><!-- wp:ainoblocks/grid-item {"gridColumnStartDesktop":1,"gridColumnEndDesktop":5,"gridColumnStartTablet":1,"gridColumnEndTablet":5,"gridColumnEndMobile":7,"marginTopTablet":12} -->
+	<div class="wp-block-ainoblocks-grid-item col_start_d__1 col_end_d__5 col_start_t__1 col_end_t__5 col_start_m__1 col_end_m__7 no-stacking mt_t__12"><!-- wp:image {"id":26842,"sizeSlug":"full","linkDestination":"none","borderRadius":"s"} -->
+	<figure class="wp-block-image size-full br-all__s"><img src="' . AINOBLOCKS_URL . 'assets/images/variant/16_9-portrait.jpg" alt="" class="wp-image-26842"/></figure>
+	<!-- /wp:image -->
+	
+	<!-- wp:image {"id":26842,"sizeSlug":"full","linkDestination":"none","borderRadius":"s","paddingTop":7} -->
+	<figure class="wp-block-image size-full br-all__s pt__7"><img src="' . AINOBLOCKS_URL . 'assets/images/variant/16_9-portrait.jpg" alt="" class="wp-image-26842"/></figure>
+	<!-- /wp:image --></div>
+	<!-- /wp:ainoblocks/grid-item -->
+	
+	<!-- wp:ainoblocks/grid-item {"gridColumnStartDesktop":5,"gridColumnEndDesktop":9,"gridColumnStartTablet":5,"gridColumnEndTablet":9,"gridColumnStartMobile":7,"marginTopDesktop":0,"marginTopTablet":0,"marginTopMobile":9} -->
+	<div class="wp-block-ainoblocks-grid-item col_start_d__5 col_end_d__9 col_start_t__5 col_end_t__9 col_start_m__7 col_end_m__13 no-stacking mt_d__0 mt_t__0 mt_m__9"><!-- wp:image {"id":26842,"sizeSlug":"full","linkDestination":"none","borderRadius":"s"} -->
+	<figure class="wp-block-image size-full br-all__s"><img src="' . AINOBLOCKS_URL . 'assets/images/variant/16_9-portrait.jpg" alt="" class="wp-image-26842"/></figure>
+	<!-- /wp:image -->
+	
+	<!-- wp:image {"id":26842,"sizeSlug":"full","linkDestination":"none","borderRadius":"s","paddingTop":7} -->
+	<figure class="wp-block-image size-full br-all__s pt__7"><img src="' . AINOBLOCKS_URL . 'assets/images/variant/16_9-portrait.jpg" alt="" class="wp-image-26842"/></figure>
+	<!-- /wp:image --></div>
+	<!-- /wp:ainoblocks/grid-item -->
+	
+	<!-- wp:ainoblocks/grid-item {"gridColumnStartDesktop":9,"gridColumnEndDesktop":13,"gridColumnStartTablet":9,"gridColumnEndTablet":13,"gridColumnEndMobile":7,"marginTopTablet":12,"marginTopMobile":-7,"className":"mobile-hide tablet-show"} -->
+	<div class="wp-block-ainoblocks-grid-item col_start_d__9 col_end_d__13 col_start_t__9 col_end_t__13 col_start_m__1 col_end_m__7 no-stacking mt_t__12 mt_m__-7 mobile-hide tablet-show"><!-- wp:image {"id":26842,"sizeSlug":"full","linkDestination":"none","borderRadius":"s"} -->
+	<figure class="wp-block-image size-full br-all__s"><img src="' . AINOBLOCKS_URL . 'assets/images/variant/16_9-portrait.jpg" alt="" class="wp-image-26842"/></figure>
+	<!-- /wp:image -->
+	
+	<!-- wp:image {"id":26842,"sizeSlug":"full","linkDestination":"none","borderRadius":"s","paddingTop":7} -->
+	<figure class="wp-block-image size-full br-all__s pt__7"><img src="' . AINOBLOCKS_URL . 'assets/images/variant/16_9-portrait.jpg" alt="" class="wp-image-26842"/></figure>
+	<!-- /wp:image --></div>
+	<!-- /wp:ainoblocks/grid-item --></div>
+	<!-- /wp:ainoblocks/grid-container --></div>
+	<!-- /wp:group -->',
+);
